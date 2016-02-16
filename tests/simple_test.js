44371677b6df62fc01a5af6e7dc8f175b00cea3d
Markus Felten <Markus.Felten@gmx.de>
2016-02-16 23:53:12 +0100

    feat: use registry-mixin to connect entities

---
+++ tests/simple_test.js
@@ -0,0 +1,24 @@
+/* global describe, it, xit, before, after */
+/* jslint node: true, esnext: true */
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+const model = require('../lib/model.js');
+
+describe('model', () => {
+  const m = new model.Model('myModel');
+  m.registerEntity(new model.Entity('e1'));
+  m.registerEntity(new model.Entity('e2')).then(
+    () => {
+      new model.Relation('e1_e2', m.entities.e1, m.entities.e2);
+    }
+  );
+
+
+  it('has name', () => assert.equal(m.name, 'myModel'));
+  it('has entity', () => assert.equal(m.entities.e1.name, 'e1'));
+});
