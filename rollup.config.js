0056410b47297607bbc921394f194f7ba94c7f72
Markus Felten <Markus.Felten@gmx.de>
2020-01-31 20:12:40 +0100

    chore(scripts): add rollup -c (scripts.prepare) chore(scripts): add rollup -c -w (scripts.start) fix(package): add arlac77/mf-styling (dependencies.mf-styling) fix(package): add ^1.3.0 (dependencies.svelte-common) fix(package): add ^1.1.11 (dependencies.svelte-guard-history-router) fix(package): add ^1.0.5 (dependencies.svelte-session-manager) chore(package): add ^1.30.1 remove ^1.25.0 (devDependencies.rollup) chore(package): add ^1.5.2 remove ^1.5.1 (devDependencies.rollup-plugin-executable) chore(package): add ^3.3.0 (devDependencies.rollup-plugin-copy) chore(package): add ^1.1.1 (devDependencies.rollup-plugin-dev) chore(package): add ^5.1.1 (devDependencies.rollup-plugin-svelte) chore(package): add ^5.2.0 (devDependencies.rollup-plugin-terser) chore(package): add ^3.18.1 (devDependencies.svelte) chore(package): add ^3.1.0 (devDependencies.builtin-modules) chore(package): add ^1.0.1 (devDependencies.rollup-plugin-consts) chore(package): add ^6.11.4 (devDependencies.npm-pkgbuild) chore(package): add ^5.0.0 (devDependencies.@semantic-release/exec) chore(package): (release) chore(package): (config) chore(package): (pacman) chore(git): update .gitignore from template chore(npm): update .npmignore from template chore(rollup): add to output sourcemap chore(rollup): import svelte,terser,dev,copy,consts,name chore(rollup): add consts,copy,svelte,resolve,commonjs,dev chore: add missing pacman/hooks.sh from template chore: add missing pacman/nginx.conf from template chore: add missing public/index.html from template chore: add missing public/manifest.json from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,11 @@
+import { name, description, version, config } from "./package.json";
+
+import consts from "rollup-plugin-consts";
+import copy from "rollup-plugin-copy";
+import dev from "rollup-plugin-dev";
+import { terser } from "rollup-plugin-terser";
+
+import svelte from "rollup-plugin-svelte";
 import builtins from "builtin-modules";
 import json from "rollup-plugin-json";
 import cleanup from 'rollup-plugin-cleanup';
@@ -7,11 +15,30 @@ import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
-  plugins: [],
+  plugins: [consts({
+    name,
+    version,
+    description,
+    ...config
+  }), copy({
+    targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
+  }), svelte({
+    dev: !production,
+    css: css => {
+      css.write(`${dist}/bundle.css`);
+    }
+  }), resolve({ browser: true }), commonjs(), dev({
+    port,
+    dirs: [dist],
+    spa: `${dist}/index.html`,
+    basePath: config.base,
+    proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
+  })],
   input: pkg.module,
 
   output: {
     format: 'cjs',
-    file: pkg.main
+    file: pkg.main,
+    sourcemap: true
   }
 };
